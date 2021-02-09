3d40bb8bbb610e95056be33130c7e3f74405782b
Elmin Nurkicm <nurkicelmin@hotmail.com>
2021-02-09 15:46:23 +0100

    Done the about section and about.service, just styles left and putting navigation

--- src/app/shared/members.ts
+++ src/app/shared/members.ts
@@ -4,32 +4,32 @@ export const MEMBERS: Member[] = [
   {
     name: 'Elmin Nurkic',
     age: 29,
-    image: '',
+    image: 'https://static.wikia.nocookie.net/penguinsofmadagascar/images/f/f8/Ahkjmort.png/revision/latest/scale-to-width-down/434?cb=20190727144938',
     created: 'About Section',
-    description: 'Lorem ',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut nemo error officiis, velit aperiam sunt fuga distinctio inventore commodi accusantium eos quasi quidem? Amet ipsam natus veritatis qui exercitationem corporis. ',
   },
 
   {
-    name: 'Daniel',
+    name: 'Daniel Najafizadeh',
     age: 32,
-    image: '',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ebayimg.com%2F19%2F!B7Vov4QEGk~%24(KGrHqQOKigEyWsHy9nzBMz3M-g7Qw~~_35.JPG%3Fset_id%3D8800005007&f=1&nofb=1',
     created: 'Contact Section',
-    description: 'Lorem ',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut nemo error officiis, velit aperiam sunt fuga distinctio inventore commodi accusantium eos quasi quidem? Amet ipsam natus veritatis qui exercitationem corporis.',
   },
 
   {
-    name: 'Erik',
-    age: 29,
-    image: '',
+    name: 'Erik Heinmert',
+    age: 23,
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.0YJ-qv0Lj__cD7ax86j7DwHaFj%26pid%3DApi&f=1',
     created: 'Form Section',
-    description: 'Lorem ',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut nemo error officiis, velit aperiam sunt fuga distinctio inventore commodi accusantium eos quasi quidem? Amet ipsam natus veritatis qui exercitationem corporis. ',
   },
 
   {
-    name: 'Marco',
-    age: 29,
-    image: '',
+    name: 'Marco Jankovic Svensson',
+    age: 23,
+    image: 'https://pbs.twimg.com/profile_images/758234370/madagascar_marty_400x400.gif',
     created: 'Home Section',
-    description: 'Lorem ',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut nemo error officiis, velit aperiam sunt fuga distinctio inventore commodi accusantium eos quasi quidem? Amet ipsam natus veritatis qui exercitationem corporis.',
   },
 ];
